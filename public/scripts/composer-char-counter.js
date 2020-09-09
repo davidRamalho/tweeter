286cb4e35eb519798facc43db9c032b77119f38a
David Ramalho <david.m.ramalho@gmail.com>
2020-09-09 00:20:31 +0000

    added character counter + started styling of the tweets container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  let length = 140; 
+  const textArea = $('#tweet-text');
+  const counter = $('.counter');
+
+  counter.text(length); 
+
+  textArea.on ('keyup', function () {
+    const chars = $(this).val().length;
+    counter.html(length - chars);
+
+    if (length < chars) {
+      counter.addClass('red');
+    } else {
+      counter.removeClass('red');
+    }
+  })
+});
