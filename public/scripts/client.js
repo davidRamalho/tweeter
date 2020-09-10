f9ab2e0ca1d7df899ceb0613de6c00688a1af7f7
David Ramalho <david.m.ramalho@gmail.com>
2020-09-10 02:02:27 +0000

    created functions to update timer and escape unsafe chars

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready( function () {
+  // converts the timestamp into a time since format
+  const timeSince = function(date) {
+    date = new Date(date);
+    
+    const seconds = Math.floor((new Date() - date) / 1000);
+    let intervalType;
   
+    let interval = Math.floor(seconds / 31536000);
+    if (interval >= 1) {
+      intervalType = 'year';
+    } else {
+      interval = Math.floor(seconds / 2592000);
+      if (interval >= 1) {
+        intervalType = 'month';
+      } else {
+        interval = Math.floor(seconds / 86400);
+        if (interval >= 1) {
+          intervalType = 'day';
+        } else {
+          interval = Math.floor(seconds / 3600);
+          if (interval >= 1) {
+            intervalType = "hour";
+          } else {
+            interval = Math.floor(seconds / 60);
+            if (interval >= 1) {
+              intervalType = "minute";
+            } else {
+              interval = seconds;
+              intervalType = "second";
+            }
+          }
+        }
+      }
+    }
+  
+    if (interval > 1 || interval === 0) {
+      intervalType += 's';
+    }
+  
+    return interval + ' ' + intervalType;
+  };
+
+  //escapes unsafe characters
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
   //takes in an object *tweet* and returns a tweet <article>  
   const createTweetElement = function (tweet) {
     let output = $(`<section id="tweets-container">
       <article class="tweet">
         <header class="tweet-header">
-          <span><img src=${tweet.user.avatars}>${tweet.user.name}</span>
-          <span id="handle">${tweet.user.handle}</span>
+          <span><img src=${tweet.user.avatars}>${escape(tweet.user.name)}</span>
+          <span id="handle">${escape(tweet.user.handle)}</span>
         </header>
         <section class="tweet-section">
-          <p id="text">${tweet.content.text}</p>
+          <div id="text">${escape(tweet.content.text)}</div>
         </section>
         <footer class="tweet-footer">
-          <p>${tweet.created_at}</p>
+          <p>${timeSince(tweet.created_at)} ago</p>
           <div class="icons"><i class="fas fa-flag fa-xs"></i>
             <i class="far fa-retweet fa-xs"></i>
             <i class="far fa-heart fa-xs"></i></div>
@@ -26,6 +74,11 @@ $(document).ready( function () {
     </section>`);
 
     return output;
+  }
+
+  //Displays Error Message
+  const createErrorMessage = function (str) {
+    `<div id="error"></div>`
   }
   
   //takes in an array of objects and appends each one to the #tweets-container
