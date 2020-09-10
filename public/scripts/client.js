269efb5601131a627826fad633fd946b3fb343b4
David Ramalho <david.m.ramalho@gmail.com>
2020-09-10 00:50:05 +0000

    added conditionals to the post request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,22 +5,9 @@
  */
 $(document).ready( function () {
   
-  //test tweet data 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
- }
-
   //takes in an object *tweet* and returns a tweet <article>  
   const createTweetElement = function (tweet) {
-    const output = $(`<section id="tweets-container">
+    let output = $(`<section id="tweets-container">
       <article class="tweet">
         <header class="tweet-header">
           <span><img src=${tweet.user.avatars}>${tweet.user.name}</span>
@@ -41,9 +28,50 @@ $(document).ready( function () {
     return output;
   }
   
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('#tweets-container').prepend($tweet); 
-   
+  //takes in an array of objects and appends each one to the #tweets-container
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      $('#tweets-container').prepend(createTweetElement(tweet));
+    }
+  }
+  
+  //Fetches the tweets stored in /tweets 
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: (tweets) => {
+        renderTweets(tweets);
+      }, 
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  };
+  
+
+  //Submits a POST request with the new-tweet information to the server and upon success calls the loadTweets function.
+  const $postTweet = $('#form');
+  $postTweet.on('submit', function (event) {
+    event.preventDefault();
+    if ($('#tweet-text').val().length > 140) {
+      alert('My little lungs can only tweet so much! 🐦');
+    } else if ( $('#tweet-text').val().length === 0) {
+      alert('A tweet cannot be empty 🐦');
+    } else {
+      const serializedData = $(this).serialize();
+      $.post('/tweets', serializedData)
+      .then((response) => {
+        loadTweets();
+        $('#tweet-text').val(''); 
+        $('.counter').val(140);
+      })
+    }
+    
+  });
+  
+  
+
 });
 
