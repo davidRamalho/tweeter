9f7acaad51427ea4a0d41601e3af5b704fc973d2
David Ramalho <david.m.ramalho@gmail.com>
2020-09-11 15:34:02 +0000

    changed toggler button to remove alerts

--- public/scripts/text-toggler.js
+++ public/scripts/text-toggler.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
     if ($('.new-tweet').hasClass('hidden')) {
       $('.new-tweet').slideDown().removeClass('hidden');
       $('#tweet-text').focus().select();
+      $('.alert').remove().slideUp(100);
     } else {
       $('.new-tweet').slideUp().addClass('hidden');
+      $('.alert').remove().slideUp(100);
     }
   });
 });
