c4599bee15cfd43f165c5a4078782900aaf3e6d0
David Ramalho <david.m.ramalho@gmail.com>
2020-09-11 05:19:13 +0000

    added slide animation to text-toggler

--- public/scripts/text-toggler.js
+++ public/scripts/text-toggler.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('#new').on('click', function() {
     if ($('.new-tweet').hasClass('hidden')) {
-      $('.new-tweet').removeClass('hidden');
+      $('.new-tweet').slideDown().removeClass('hidden');
       $('#tweet-text').focus().select();
     } else {
-      $('.new-tweet').addClass('hidden');
+      $('.new-tweet').slideUp().addClass('hidden');
     }
   });
 });
